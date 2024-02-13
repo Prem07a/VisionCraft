7ca34ab1115a2ba99070362dc479cdc8189c669f
Prem <gen.premganeshgaikwad@gmail.com>
2024-02-13 23:19:06 +0530

    Update

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
-DESCRIPTION = 'Computer Vision Library to help do CV much faster'
+VERSION = '0.1.0'
+DESCRIPTION = 'Computer Vision Library to help do CV much faster, Added new Help Feature'
 LONG_DESCRIPTION = 'Simplifying computer vision tasks with Python. Visualize, transform, and analyze images effortlessly. Ideal for students, researchers, and developers. Enhance your computer vision projects!'
 
 # Setting up
